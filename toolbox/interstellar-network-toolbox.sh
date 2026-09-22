a04ad5856316aeced7f74fad8f65cf322e979b91
Kees Kemper <kees.kemper@basic-fit.com>
2026-09-22 12:25:32 +0200

    Release v4.3.1

--- toolbox/interstellar-network-toolbox.sh
+++ toolbox/interstellar-network-toolbox.sh
@@ -5,7 +5,7 @@ set -Eeuo pipefail
 # Supports Debian and Ubuntu.
 # Start without arguments for the interactive menu.
 
-VERSION="4.3.0"
+VERSION="4.3.1"
 BACKUP_DIR="/var/backups/interstellar-toolbox"
 SSH_DROPIN="/etc/ssh/sshd_config.d/99-interstellar-hardening.conf"
 MANAGER_INSTALL_PATH="/usr/local/sbin/interstellar-toolbox"
